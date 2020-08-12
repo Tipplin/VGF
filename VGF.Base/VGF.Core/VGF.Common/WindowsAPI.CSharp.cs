6dce81619e9421393bc14b9dd3bdd402651a153c
Kurs Christian <40143278+Tipplin@users.noreply.github.com>
2020-08-12 15:45:36 +0200

    Update WindowsAPI.CSharp.cs

--- VGF.Base/VGF.Core/VGF.Common/WindowsAPI.CSharp.cs
+++ VGF.Base/VGF.Core/VGF.Common/WindowsAPI.CSharp.cs
@@ -113,10 +113,10 @@ static extern uint SearchPath (
 
 
 
-public bool scpSearchFile(string FileName)
+public bool VGFSearchAnFile(string FileName)
 {
 	/// Set StringBuilder faster as string
-	StringBuilder sb = new StringBuilder ( 260 );
+	var sb = new StringBuilder ( 260 );
 	IntPtr ptr = new IntPtr ( );
 	
 	SearchPath ( null ,
@@ -138,67 +138,67 @@ public bool scpSearchFile(string FileName)
 ///
 ///-------------------------------------------------
 [Flags]
- public enum FileSystemFeature : uint {
+ public enum VGFFileSystemOSFeature : uint {
      /// <summary>
      /// The file system supports case-sensitive file names.
      /// </summary>
-     CaseSensitiveSearch = 1,
+     VGFCaseSensitiveSearch = 1,
      /// <summary>
      /// The file system preserves the case of file names when it places a name on disk.
      /// </summary>
-     CasePreservedNames = 2,
+     VGFCasePreservedNames = 2,
      /// <summary>
      /// The file system supports Unicode in file names as they appear on disk.
      /// </summary>
-     UnicodeOnDisk = 4,
+     VGFUnicodeOnDisk = 4,
      /// <summary>
      /// The file system preserves and enforces access control lists (ACL).
      /// </summary>
-     PersistentACLS = 8,
+     VGFPersistentACLS = 8,
      /// <summary>
      /// The file system supports file-based compression.
      /// </summary>
-     FileCompression = 0x10,
+     VGFFileCompression = 0x10,
      /// <summary>
      /// The file system supports disk quotas.
      /// </summary>
-     VolumeQuotas = 0x20,
+     VGFVolumeQuotas = 0x20,
      /// <summary>
      /// The file system supports sparse files.
      /// </summary>
-     SupportsSparseFiles = 0x40,
+     VGFSupportsSparseFiles = 0x40,
      /// <summary>
      /// The file system supports re-parse points.
      /// </summary>
-     SupportsReparsePoints = 0x80,
+     VGFSupportsReparsePoints = 0x80,
      /// <summary>
      /// The specified volume is a compressed volume, for example, a DoubleSpace volume.
      /// </summary>
-     VolumeIsCompressed = 0x8000,
+     VGFVolumeIsCompressed = 0x8000,
      /// <summary>
      /// The file system supports object identifiers.
      /// </summary>
-     SupportsObjectIDs = 0x10000,
+     VGFSupportsObjectIDs = 0x10000,
      /// <summary>
      /// The file system supports the Encrypted File System (EFS).
      /// </summary>
-     SupportsEncryption = 0x20000,
+     VGFSupportsEncryption = 0x20000,
      /// <summary>
      /// The file system supports named streams.
      /// </summary>
-     NamedStreams = 0x40000,
+     VGFNamedStreams = 0x40000,
      /// <summary>
      /// The specified volume is read-only.
      /// </summary>
-     ReadOnlyVolume = 0x80000,
+     VGFReadOnlyVolume = 0x80000,
      /// <summary>
      /// The volume supports a single sequential write.
      /// </summary>
-     SequentialWriteOnce = 0x100000,
+     VGFSequentialWriteOnce = 0x100000,
      /// <summary>
      /// The volume supports transactions.
      /// </summary>
-     SupportsTransactions = 0x200000,
+     VGFSupportsTransactions = 0x200000,
  }
 
 
@@ -220,8 +220,8 @@ internal extern static bool GetVolumeInformation(
                                                      int nFileSystemNameSize);
 
 
-StringBuilder volname = new StringBuilder(261);
- StringBuilder fsname = new StringBuilder(261);
+ var volname = new StringBuilder(261);
+ var fsname = new StringBuilder(261);
  uint sernum, maxlen;
  FileSystemFeature flags;
  if(!GetVolumeInformation("c:\\", volname, volname.Capacity, out sernum, out maxlen, out flags, fsname, fsname.Capacity))
@@ -231,7 +231,7 @@ StringBuilder volname = new StringBuilder(261);
 
 
 
-public string scpGetVolumeSerial(string DriveLetter)
+public string VGFGetVolumeSerial(string DriveLetter)
 {
 	/// Check for valid drive letter argument. 
 	const string ValidDriveLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -302,7 +302,7 @@ enum COMPUTER_NAME_FORMAT
 }
 
 
-public string scpGetComputerDNSDomainName()
+public string VGFGetComputerDNSDomainName()
 {
 	bool success;
 
@@ -322,7 +322,7 @@ public string scpGetComputerDNSDomainName()
 ///
 /// ComputerNamePhysicalDnsHostname used to rename the computer name and netbios name before domain join
 ///
-public static bool scpSetComputerPhysDNSHostName(string name)
+public static bool VGFSetComputerPhysDNSHostName(string name)
 {
          try
          {
